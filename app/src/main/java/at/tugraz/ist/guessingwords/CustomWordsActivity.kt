e3ffc8602100e33893c9a1a398ef2d2523814e54
Khalid Nasralla <khalid.nasralla@student.tugraz.at>
2021-04-15 14:25:26 +0200

    GW-003-C added input textfield and save button [NK, TD, BH]

--- app/src/main/java/at/tugraz/ist/guessingwords/CustomWordsActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/CustomWordsActivity.kt
@@ -3,6 +3,7 @@ package at.tugraz.ist.guessingwords
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class CustomWordsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,11 +11,19 @@ class CustomWordsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_custom_words)
 
         val btn_back = findViewById(R.id.btn_back_CW) as Button
+        val btn_save = findViewById(R.id.btn_save_word) as Button
+        val text_field = findViewById(R.id.editText_customWords) as EditText
 
         btn_back.setOnClickListener {
             finish()
-
         }
 
+
+        val custom_words = mutableListOf<String>()
+
+        btn_save.setOnClickListener {
+            var editTextString = text_field.text.toString()
+            custom_words.add(editTextString) // saves the custom words into list for now, until database is ready
+        }
     }
 }
