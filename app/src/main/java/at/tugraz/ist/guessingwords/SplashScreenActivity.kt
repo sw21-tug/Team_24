a9a9eb561277401878cdfd45b43cd11467e8304b
egirin <egirin.gega@uni-graz.at>
2021-04-18 21:12:46 +0200

    GW-001-C decrease splash screen timeout [GE, KA]

--- app/src/main/java/at/tugraz/ist/guessingwords/SplashScreenActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/SplashScreenActivity.kt
@@ -8,7 +8,7 @@ import android.os.Looper
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private var TIME_OUT:Long = 3000
+    private var TIME_OUT:Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
