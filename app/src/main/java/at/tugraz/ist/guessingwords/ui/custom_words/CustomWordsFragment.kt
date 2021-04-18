f701ffca4321e7b8790442a73227de10256e05a6
Taupe Alexandra <alexandra.taupe@student.tugraz.at>
2021-04-18 17:07:45 +0200

    GW-002-D refactored start new activity to use fragments and ViewModel [SK, TA]

---
+++ app/src/main/java/at/tugraz/ist/guessingwords/ui/custom_words/CustomWordsFragment.kt
@@ -0,0 +1,26 @@
+package at.tugraz.ist.guessingwords.ui.custom_words
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import at.tugraz.ist.guessingwords.R
+
+class CustomWordsFragment : Fragment() {
+
+    private lateinit var customWordsViewModel: CustomWordsViewModel
+    private lateinit var root: View
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val customWordsFactory: ViewModelProvider.Factory = ViewModelProvider.AndroidViewModelFactory.getInstance(activity!!.application)
+        customWordsViewModel = ViewModelProvider(this, customWordsFactory).get(CustomWordsViewModel::class.java)
+        root = inflater.inflate(R.layout.fragment_custom_words, container, false)
+        return root
+    }
+}
