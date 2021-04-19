1cfcac1a3354ea69099868309c2f32dfdb860905
Khalid Nasralla <khalid.nasralla@student.tugraz.at>
2021-04-19 13:51:25 +0200

    GW-003-C added test case for entering custom word + hide keyboard after saving [BH, NK, TD]

--- app/src/main/java/at/tugraz/ist/guessingwords/CustomWordsActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/CustomWordsActivity.kt
@@ -1,9 +1,12 @@
 package at.tugraz.ist.guessingwords
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class CustomWordsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +25,17 @@ class CustomWordsActivity : AppCompatActivity() {
         val custom_words = mutableListOf<String>()
 
         btn_save.setOnClickListener {
+            Toast.makeText(this@CustomWordsActivity, "You Clicked Save!", Toast.LENGTH_SHORT).show()
             var editTextString = text_field.text.toString()
             custom_words.add(editTextString) // saves the custom words into list for now, until database is ready
+            closeKeyBoard()
+        }
+    }
+    private fun closeKeyBoard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
 }
