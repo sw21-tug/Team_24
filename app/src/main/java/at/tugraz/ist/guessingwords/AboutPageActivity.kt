516c4c091e3cd06b4e5d94b83d94c832e23381e3
amilsalihi00 <65094404+amilsalihi00@users.noreply.github.com>
2021-04-28 16:07:03 +0200

    GW-013-B added activity, fragment and back button test [SA, TD]

---
+++ app/src/main/java/at/tugraz/ist/guessingwords/AboutPageActivity.kt
@@ -0,0 +1,15 @@
+package at.tugraz.ist.guessingwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class AboutPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_page)
+
+       
+
+    }
+}
