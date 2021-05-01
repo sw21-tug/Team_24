f477276b1b7b324af65e45376f445e1c7ad990bb
Felix Wallner <felix.wallner@student.tugraz.at>
2021-05-01 13:29:27 +0200

    GW-005-B impl. game next word logic and wrote more tests to existing logic [BH, WF]

--- app/src/androidTest/java/at/tugraz/ist/guessingwords/logic/GameTest.kt
+++ app/src/androidTest/java/at/tugraz/ist/guessingwords/logic/GameTest.kt
@@ -4,6 +4,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import at.tugraz.ist.guessingwords.data.entity.Word
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.IllegalArgumentException
 
 @RunWith(AndroidJUnit4::class)
 class GameTest {
@@ -26,11 +27,28 @@ class GameTest {
 
         val word_1 = game.getWord()
         game.next()
-
         val word_2 = game.getWord()
 
         assert(word_1 != word_2)
-
     }
 
+    @Test
+    fun testWrapAroundToSameWordAfterUsingAllWords()
+    {
+        val words = listOf<Word>(Word("hallo"))
+        val game = Game(words)
+
+        val word_1 = game.getWord()
+        game.next()
+        val word_2 = game.getWord()
+
+        assert(word_1 == word_2)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun expectIllegalArgumentExceptionOnCreatingGameWithEmptyWordList()
+    {
+        val words = listOf<Word>()
+        Game(words)
+    }
 }
--- app/src/main/java/at/tugraz/ist/guessingwords/logic/Game.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/logic/Game.kt
@@ -5,15 +5,18 @@ import java.lang.IllegalArgumentException
 
 class Game(val wordPool: List<Word>)
 {
+    private val used_indices = HashSet<Int>()
+    private var current_word: Word
 
-    private lateinit var current_word:Word
     init
     {
         if(wordPool.isEmpty())
         {
             throw IllegalArgumentException("wordpool can not be empty")
         }
-        current_word = wordPool.random()
+        val index = wordPool.indices.random()
+        used_indices.add(index)
+        current_word = wordPool[index]
     }
 
     fun getWord(): Word
@@ -23,6 +26,13 @@ class Game(val wordPool: List<Word>)
 
     fun next()
     {
-
+        if (wordPool.size == used_indices.size) {
+            // all words were used
+            used_indices.clear()
+        }
+        val usable = wordPool.indices.subtract(used_indices)
+        val index = usable.random()
+        used_indices.add(index)
+        current_word = wordPool[index]
     }
 }
