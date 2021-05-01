ef7ca42932e45531343ce99336bf297200933395
Felix Wallner <felix.wallner@student.tugraz.at>
2021-05-01 13:56:14 +0200

    GW-005-B wrote new test for game fragment expect loading msg [BH, WF]

---
+++ app/src/androidTest/java/at/tugraz/ist/guessingwords/ui/start_game/GamePrototypeFragmentTest.kt
@@ -0,0 +1,40 @@
+package at.tugraz.ist.guessingwords.ui.start_game
+
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import at.tugraz.ist.guessingwords.R
+import at.tugraz.ist.guessingwords.data.entity.Word
+import at.tugraz.ist.guessingwords.data.service.WordService
+import at.tugraz.ist.guessingwords.logic.Game
+import at.tugraz.ist.guessingwords.ui.custom_words.CustomWordsFragment
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+
+@RunWith(AndroidJUnit4::class)
+class GamePrototypeFragmentTest {
+
+    @Test
+    fun expectToSeeLoadingMessageOnScreenBeforeGameStart() {
+        val wordServiceMock = mock<WordService>()
+        var text = ""
+
+        val fragmentArgs = bundleOf()
+        val scenario = launchFragmentInContainer<GamePrototypeFragment>(fragmentArgs)
+        scenario.onFragment { fragment ->
+            text = fragment.getString(R.string.loading)
+            fragment.wordService = wordServiceMock
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.txt_fieldWord)).check(ViewAssertions.matches(ViewMatchers.withText(text)))
+    }
+}
--- app/src/main/java/at/tugraz/ist/guessingwords/ui/start_game/GamePrototypeFragment.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/ui/start_game/GamePrototypeFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import at.tugraz.ist.guessingwords.R
+import at.tugraz.ist.guessingwords.data.service.WordService
+import at.tugraz.ist.guessingwords.logic.Game
 
 class GamePrototypeFragment : Fragment() {
 
     private lateinit var startGameViewModel: StartGameViewModel
     private lateinit var root: View
+
+    lateinit var wordService: WordService
+
+    private var game: Game? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
