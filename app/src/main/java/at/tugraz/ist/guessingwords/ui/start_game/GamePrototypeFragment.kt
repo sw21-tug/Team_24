171645ecd3b0356c686f9454ac386dbc34f1766c
Hosam Badr <hosam.badr@student.tugraz.at>
2021-05-01 14:08:57 +0200

    GW-005-B implemented initFields function [BH, WF]

--- app/src/main/java/at/tugraz/ist/guessingwords/ui/start_game/GamePrototypeFragment.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/ui/start_game/GamePrototypeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -19,6 +20,11 @@ class GamePrototypeFragment : Fragment() {
     lateinit var wordService: WordService
 
     private var game: Game? = null
+    private lateinit var fieldTimer: TextView
+    private lateinit var fieldWord: TextView
+    private lateinit var fieldWordCounter: TextView
+    private lateinit var btn_skip: Button
+    private lateinit var btn_correct: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +34,22 @@ class GamePrototypeFragment : Fragment() {
         val startGameFactory: ViewModelProvider.Factory = ViewModelProvider.AndroidViewModelFactory.getInstance(activity!!.application)
         startGameViewModel = ViewModelProvider(this, startGameFactory).get(StartGameViewModel::class.java)
         root = inflater.inflate(R.layout.fragment_game_prototype, container, false)
+
+        initFields()
+
         return root
+    }
+
+    private fun initFields()
+    {
+        fieldTimer = root.findViewById(R.id.txt_fieldTimer)
+        fieldWord = root.findViewById(R.id.txt_fieldWord)
+        fieldWordCounter = root.findViewById(R.id.txt_fieldWordCounter)
+        btn_correct = root.findViewById(R.id.btn_correctWord)
+        btn_skip = root.findViewById(R.id.btn_skipWord)
+
+        fieldTimer.text = ""
+        fieldWord.text = getString(R.string.loading)
+        fieldWordCounter.text = ""
     }
 }
