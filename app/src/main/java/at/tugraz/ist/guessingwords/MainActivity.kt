a809175912b97cddbb54daa5b3191da004e1a563
Katharina Steinacher <katharina.steinacher@student.tugraz.at>
2021-05-04 21:05:39 +0200

    GW-014 implemented dialog [GE, SK]

--- app/src/main/java/at/tugraz/ist/guessingwords/MainActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/MainActivity.kt
@@ -2,15 +2,19 @@ package at.tugraz.ist.guessingwords
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
+import android.view.*
+import android.widget.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var locale: Locale
+    private var currentLanguage = "en"
+    private var currentLang: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,11 +33,33 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CustomWordsActivity::class.java)
             startActivity(intent)
         }
+        currentLanguage = intent.getStringExtra(currentLang).toString()
     }
 
     override fun onResume() {
         super.onResume()
 //        Toast.makeText(this@MainActivity, "Resumed from other Activity", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setLocale(localeName: String) {
+        if (localeName != currentLanguage) {
+            locale = Locale(localeName)
+            val res = resources
+            val dm = res.displayMetrics
+            val conf = res.configuration
+            conf.locale = locale
+            res.updateConfiguration(conf, dm)
+            val refresh = Intent(
+                    this,
+                    MainActivity::class.java
+            )
+            refresh.putExtra(currentLang, localeName)
+            refresh.putExtra(currentLanguage, localeName)
+            startActivity(refresh)
+        } else {
+            Toast.makeText(
+                    this@MainActivity, "Language is already selected!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -47,9 +73,40 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AboutPageActivity::class.java)
             startActivity(intent)
             return true
+        } else if(item.itemId == R.id.btn_change_language) {
+            openLanguageDialog();
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    @Override
+    fun openLanguageDialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.fragment_languages_dialog, null)
+        val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle(R.string.lang_dialog_title)
+        var setSelected:View
+
+        if (currentLanguage != "ru") {
+            setSelected = mDialogView.findViewById<RadioButton>(R.id.lang_english)
+        } else {
+            setSelected = mDialogView.findViewById<RadioButton>(R.id.lang_russian)
+        }
+        setSelected.setChecked(true);
+        val mAlertDialog = mBuilder.show()
+        val button = mDialogView.findViewById<Button>(R.id.btn_language_change)
+        button.setOnClickListener{
+            var selected = (mDialogView.findViewById<RadioGroup>(R.id.lang_radio).checkedRadioButtonId)
+            var text = mDialogView.findViewById<RadioButton>(selected).text
+            if (text == "English"){
+                setLocale("en")
+            } else if(text == "Russian") {
+                setLocale("ru")
+            }
+            mAlertDialog.dismiss()
+        }
+
     }
 
 }
