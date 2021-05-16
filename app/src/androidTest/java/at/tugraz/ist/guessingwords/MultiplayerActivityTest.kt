c3c0da9409a4939655717afec7654c14412a8415
Khalid Nasralla <khalid.nasralla@student.tugraz.at>
2021-05-16 13:03:00 +0200

    GW-017 added test for multiplayer names [KA, NK]

---
+++ app/src/androidTest/java/at/tugraz/ist/guessingwords/MultiplayerActivityTest.kt
@@ -0,0 +1,24 @@
+package at.tugraz.ist.guessingwords
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Test
+
+class MultiplayerActivityTest {
+
+
+    @Test
+    fun addMultiplayerText() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val input = "Testing multiplayer txt box"
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).check(ViewAssertions.matches(ViewMatchers.isClickable()))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.editText_multiplayer)).perform(ViewActions.typeText(input))
+
+        //Espresso.onView(ViewMatchers.withId(R.id.li_customWord_text)).check(ViewAssertions.matches(ViewMatchers.withText(input)))
+    }
+}
