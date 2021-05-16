b86ddaceec3b6237f86184b0a116ba7d9d75eb60
Khalid Nasralla <khalid.nasralla@student.tugraz.at>
2021-05-16 15:04:30 +0200

    GW-017 implemented host activity and added ready button [KA, NK]

--- app/src/main/java/at/tugraz/ist/guessingwords/HostActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/HostActivity.kt
@@ -1,11 +1,16 @@
 package at.tugraz.ist.guessingwords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
+
+
     }
 }
--- app/src/main/java/at/tugraz/ist/guessingwords/MultiplayerActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/MultiplayerActivity.kt
@@ -1,8 +1,10 @@
 package at.tugraz.ist.guessingwords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MultiplayerActivity : AppCompatActivity() {
 
@@ -12,5 +14,11 @@ class MultiplayerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_multiplayer)
 
         val btn_host = findViewById<Button>(R.id.btn_host)
+
+        btn_host.setOnClickListener {
+            Toast.makeText(this@MultiplayerActivity, "You Clicked: Ready!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
