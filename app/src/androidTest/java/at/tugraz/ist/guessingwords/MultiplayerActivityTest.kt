5772ba5be6b16eeb148f44d75556b52a358f166d
zauberer1 <62005784+zauberer1@users.noreply.github.com>
2021-05-19 11:15:26 +0200

    GW-017 added test for multiplayer back button [KA, NK]

--- app/src/androidTest/java/at/tugraz/ist/guessingwords/MultiplayerActivityTest.kt
+++ app/src/androidTest/java/at/tugraz/ist/guessingwords/MultiplayerActivityTest.kt
@@ -5,6 +5,7 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Test
 
 class MultiplayerActivityTest {
@@ -37,4 +38,14 @@ class MultiplayerActivityTest {
         Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).perform(ViewActions.click())
         Espresso.onView(ViewMatchers.withId(R.id.btn_join)).check(ViewAssertions.matches(ViewMatchers.isClickable()))
     }
+
+    @Test
+    fun multiplayerBackButton(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).check(ViewAssertions.matches(ViewMatchers.isClickable()))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withContentDescription(R.string.abc_action_bar_up_description)).perform(ViewActions.click())
+    }
+
 }
