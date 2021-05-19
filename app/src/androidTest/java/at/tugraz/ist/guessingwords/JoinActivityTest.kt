6c324989f6c2dc4323d10bc07d4abeb5241c7b70
Khalid Nasralla <khalid.nasralla@student.tugraz.at>
2021-05-19 10:17:30 +0200

    GW-017 added test for join activity screen [KA, NK]

---
+++ app/src/androidTest/java/at/tugraz/ist/guessingwords/JoinActivityTest.kt
@@ -0,0 +1,22 @@
+package at.tugraz.ist.guessingwords
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Test
+
+class JoinActivityTest {
+
+    @Test
+    fun joinScreenMessageCheck(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).check(ViewAssertions.matches(ViewMatchers.isClickable()))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_multiplayer)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.btn_join)).check(ViewAssertions.matches(ViewMatchers.isClickable()))
+        Espresso.onView(ViewMatchers.withId(R.id.btn_join)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.text_wordsSentMessage)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
