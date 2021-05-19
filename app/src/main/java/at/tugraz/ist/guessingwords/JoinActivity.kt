1e9c539ba83397ed4a0751ac5d9ce4c717354553
Khalid Nasralla <khalid.nasralla@student.tugraz.at>
2021-05-19 12:21:26 +0200

    GW-017 implemented join screen back button [KA, NK]

--- app/src/main/java/at/tugraz/ist/guessingwords/JoinActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/JoinActivity.kt
@@ -2,10 +2,26 @@ package at.tugraz.ist.guessingwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        customizeActionBar()
+    }
+
+    private fun customizeActionBar() {
+        supportActionBar?.title = getString(R.string.gameplay)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return true
     }
 }
