07da0f57dfd5e91efbe61dd87f2b2a60debf306b
zauberer1 <62005784+zauberer1@users.noreply.github.com>
2021-05-19 12:08:42 +0200

    GW-017 implemented the host screen back button [KA, NK]

--- app/src/main/java/at/tugraz/ist/guessingwords/HostActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/HostActivity.kt
@@ -3,6 +3,7 @@ package at.tugraz.ist.guessingwords
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 
@@ -11,6 +12,20 @@ class HostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
 
+        customizeActionBar()
+    }
 
+    private fun customizeActionBar() {
+        supportActionBar?.title = getString(R.string.gameplay)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return true
     }
 }
