1dba9fd62cfcecee535e996343f0427b03a33ca6
Alexandra Taupe <alexandra.taupe@student.tugraz.at>
2021-06-02 16:33:13 +0200

    Gw-007 established P2P connection between two devices [TA, WF]

--- app/src/main/java/at/tugraz/ist/guessingwords/HostActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/HostActivity.kt
@@ -1,18 +1,106 @@
 package at.tugraz.ist.guessingwords
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.Toast
+import com.adroitandroid.near.connect.NearConnect
+import com.adroitandroid.near.discovery.NearDiscovery
+import com.adroitandroid.near.model.Host
 
 class HostActivity : AppCompatActivity() {
+
+    private lateinit var nearDiscovery: NearDiscovery
+    private lateinit var nearConnect: NearConnect
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
-
         customizeActionBar()
+
+        val name = "GW-host"+(1..5000).random()
+        val searchingFilter = "GW-join.*"
+        val identifyingFilter = "GW-host.*"
+
+        nearDiscovery = NearDiscovery.Builder()
+                .setContext(this)
+                .setDiscoverableTimeoutMillis(60000)
+                .setDiscoveryTimeoutMillis(60000)
+                .setDiscoverablePingIntervalMillis(1000)
+                .setDiscoveryListener(nearDiscoveryListener, Looper.getMainLooper())
+                .setFilter(Regex(searchingFilter))
+                .build()
+
+        nearConnect = NearConnect.Builder()
+                .fromDiscovery(nearDiscovery)
+                .setContext(this)
+                .setListener(nearConnectListener, Looper.getMainLooper())
+                .build()
+
+        Log.d("HostActivity", "Starting discovery... as $name")
+        Toast.makeText(this@HostActivity, "Starting discovery... as $name", Toast.LENGTH_LONG).show()
+        nearDiscovery.makeDiscoverable(name, identifyingFilter)
+        nearDiscovery.startDiscovery()
+        nearConnect.startReceiving()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopNearServices()
+    }
+
+    private val nearDiscoveryListener: NearDiscovery.Listener
+        get() = object : NearDiscovery.Listener {
+            override fun onPeersUpdate(host: Set<Host>) {
+                Log.d("HostActivity", "Peer Update. Found ${host.size} hosts")
+                Toast.makeText(this@HostActivity, "Peer Update. Found ${host.size} hosts", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onDiscoveryTimeout() {
+                Log.d("HostActivity", "No other participants found")
+                Toast.makeText(this@HostActivity, "No other participants found", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onDiscoveryFailure(e: Throwable) {
+                Log.d("HostActivity", "Something went wrong while searching for participants")
+                Toast.makeText(this@HostActivity, "Something went wrong while searching for participants", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onDiscoverableTimeout() {
+                Log.d("HostActivity", "You're not discoverable anymore")
+                Toast.makeText(this@HostActivity, "You're not discoverable anymore", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private val nearConnectListener: NearConnect.Listener
+        get() = object : NearConnect.Listener {
+            override fun onReceive(bytes: ByteArray, sender: Host) {
+                Toast.makeText(this@HostActivity, "Received text: ${String(bytes)}", Toast.LENGTH_LONG).show()
+                Log.d("HostActivity", String(bytes))
+            }
+
+            override fun onSendComplete(jobId: Long) {}
+            override fun onSendFailure(e: Throwable?, jobId: Long) {
+                Toast.makeText(this@HostActivity, "onSendFailure", Toast.LENGTH_LONG).show()
+                Log.d("HostActivity", "onSendFailure")
+            }
+            override fun onStartListenFailure(e: Throwable?) {
+                Toast.makeText(this@HostActivity, "onStartListenFailure", Toast.LENGTH_LONG).show()
+                Log.e("HostActivity", "onStartListenFailure")
+                Log.e("HostActivity", e.toString())
+            }
+        }
+
+    private fun stopDiscovery() {
+        nearDiscovery.makeNonDiscoverable()
+        nearDiscovery.stopDiscovery()
+    }
+
+    private fun stopNearServices() {
+        nearConnect.stopReceiving(true)
+        stopDiscovery()
     }
 
     private fun customizeActionBar() {
--- app/src/main/java/at/tugraz/ist/guessingwords/JoinActivity.kt
+++ app/src/main/java/at/tugraz/ist/guessingwords/JoinActivity.kt
@@ -2,14 +2,112 @@ package at.tugraz.ist.guessingwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
+import com.adroitandroid.near.connect.NearConnect
+import com.adroitandroid.near.discovery.NearDiscovery
+import com.adroitandroid.near.model.Host
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var nearDiscovery: NearDiscovery
+    private lateinit var nearConnect: NearConnect
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
         customizeActionBar()
+
+        val name = "GW-join"+(1..5000).random()
+        val searchingFilter = "GW-host.*"
+        val identifyingFilter = "GW-join.*"
+
+        nearDiscovery = NearDiscovery.Builder()
+                .setContext(this)
+                .setDiscoverableTimeoutMillis(60000)
+                .setDiscoveryTimeoutMillis(60000)
+                .setDiscoverablePingIntervalMillis(1000)
+                .setDiscoveryListener(nearDiscoveryListener, Looper.getMainLooper())
+                .setFilter(Regex(searchingFilter))
+                .build()
+
+        nearConnect = NearConnect.Builder()
+                .fromDiscovery(nearDiscovery)
+                .setContext(this)
+                .setListener(nearConnectListener, Looper.getMainLooper())
+                .build()
+
+
+        Log.d("JoinActivity", "Starting discovery... as $name")
+        Toast.makeText(this@JoinActivity, "Starting discovery... as $name", Toast.LENGTH_LONG).show()
+        nearDiscovery.makeDiscoverable(name, identifyingFilter)
+        nearDiscovery.startDiscovery()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopDiscovery()
+    }
+
+    private fun sendMessage(msg: String, host: Host) {
+        Log.d("JoinActivity", "Sending message: ${msg}")
+        Toast.makeText(this@JoinActivity, "Sending message: ${msg}", Toast.LENGTH_LONG).show()
+        nearConnect.send(msg.toByteArray(), host)
+    }
+
+    private val nearDiscoveryListener: NearDiscovery.Listener
+        get() = object : NearDiscovery.Listener {
+            override fun onPeersUpdate(host: Set<Host>) {
+                Log.d("JoinActivity", "Peer Update. Found ${host.size} hosts")
+                Toast.makeText(this@JoinActivity, "Peer Update. Found ${host.size} hosts", Toast.LENGTH_LONG).show()
+                stopDiscovery()
+                host.forEach {
+                    h -> sendMessage("hallo"+(1..5000).random(), h)
+                }
+            }
+
+            override fun onDiscoveryTimeout() {
+                Log.d("JoinActivity", "No other participants found")
+                Toast.makeText(this@JoinActivity, "No other participants found", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onDiscoveryFailure(e: Throwable) {
+                Log.d("JoinActivity", "Something went wrong while searching for participants")
+                Toast.makeText(this@JoinActivity, "Something went wrong while searching for participants", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onDiscoverableTimeout() {
+                Log.d("JoinActivity", "You're not discoverable anymore")
+                Toast.makeText(this@JoinActivity, "You're not discoverable anymore", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private val nearConnectListener: NearConnect.Listener
+        get() = object : NearConnect.Listener {
+            override fun onReceive(bytes: ByteArray, sender: Host) {
+                Toast.makeText(this@JoinActivity, "Received text: ${String(bytes)}", Toast.LENGTH_LONG).show()
+                Log.d("RECEIVED", String(bytes))
+            }
+
+            override fun onSendComplete(jobId: Long) {}
+            override fun onSendFailure(e: Throwable?, jobId: Long) {
+                Toast.makeText(this@JoinActivity, "onSendFailure", Toast.LENGTH_LONG).show()
+                Log.d("JoinActivity", "onSendFailure")
+            }
+            override fun onStartListenFailure(e: Throwable?) {
+                Toast.makeText(this@JoinActivity, "onStartListenFailure", Toast.LENGTH_LONG).show()
+                Log.d("JoinActivity", "onStartListenFailure")
+            }
+        }
+
+    private fun stopDiscovery() {
+        nearDiscovery.makeNonDiscoverable()
+        nearDiscovery.stopDiscovery()
+        Log.d("JoinActivity", "Stopping discovery")
+    }
+
 
     private fun customizeActionBar() {
         supportActionBar?.title = getString(R.string.gameplay)
